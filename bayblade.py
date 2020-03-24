b4150881f815a9f774d57b300ea808b9a812f3cd
NADEE KHAN <nkhan@localhost.localdomain>
2020-03-24 11:17:00 +0530

    added solution

--- bayblade.py
+++ bayblade.py
@@ -1,14 +1,14 @@
 q = int(input())
 
-size = int(input())
-
-team_a = sorted([ int(str(i).strip()) for i in str(input()).strip().split(" ")])
-
-team_b = sorted([ int(str(i).strip()) for i in str(input()).strip().split(" ")])
-
 
 
 for i in range(0, q):
+    
+    size = int(input())
+
+    team_a = sorted([ int(str(i).strip()) for i in str(input()).strip().split(" ")])
+
+    team_b = sorted([ int(str(i).strip()) for i in str(input()).strip().split(" ")])
     m_b = 0
     possible_wins = 0
     for power in team_a:
